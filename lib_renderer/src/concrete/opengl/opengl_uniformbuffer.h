2d1c8f888abc0d214bcc824f2ae9d94338aeeeac
Darragh McGurk <120114728+dmcg310@users.noreply.github.com>
2024-06-23 15:35:37 +0100

    feat: add opengl_uniformbuffer.h

---
+++ lib_renderer/src/concrete/opengl/opengl_uniformbuffer.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../../../include/buffer.h"
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+class OpenGLUniformBuffer : public Buffer {
+public:
+  void Create(size_t size, const void *data) override;
+  void Bind(GLuint bindingPoint) override;
+  void SetData(size_t size, const void *data) override;
+  ~OpenGLUniformBuffer() override;
+
+private:
+  GLuint bufferID;
+};
