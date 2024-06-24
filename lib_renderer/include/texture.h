f3dd96513e02811ba48e273133a68c98f2f5c2d0
Darragh McGurk <120114728+dmcg310@users.noreply.github.com>
2024-06-24 21:31:57 +0100

    feat: add `Create()` function to opengl texture api

--- lib_renderer/include/texture.h
+++ lib_renderer/include/texture.h
@@ -5,6 +5,7 @@
 class Texture {
 public:
   virtual void LoadFromFile(const std::string &filePath) = 0;
+  virtual void Create(int width, int height, unsigned int format) = 0;
   virtual void Bind(unsigned int unit) = 0;
   virtual unsigned int GetID() const = 0;
   virtual void SetParameters() = 0;
--- lib_renderer/src/concrete/opengl/opengl_texture.h
+++ lib_renderer/src/concrete/opengl/opengl_texture.h
@@ -8,6 +8,7 @@ class OpenGLTexture : public Texture {
 public:
   OpenGLTexture();
   void LoadFromFile(const std::string &filePath) override;
+  void Create(int width, int height, GLenum format) override;
   void Bind(unsigned int unit) override;
   void SetParameters() override;
   unsigned int GetID() const override;
